429fe946732c4b541c8ec2702504c80ef8b4ea2e
BlackMelon980 <capofrancy@gmail.com>
2024-01-10 13:03:14 +0100

    wip

--- Appetizers/Appetizer.swift
+++ Appetizers/Appetizer.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "Description from appetizer.", price: 9.99, imageURL: "", calories: 99, protein: 99, carbs: 10)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
