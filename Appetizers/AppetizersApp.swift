d5b6332cb9150c666b43c05a9713b2d50f72d6fd
francescacapodanno <francesca.capodanno@telepass.com>
2024-01-10 10:51:19 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Francesca Capodanno on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
