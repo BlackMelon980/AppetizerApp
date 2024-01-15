1eaf8f21b017baa6119f2c0e9a7b3d1998c3c541
BlackMelon980 <capofrancy@gmail.com>
2024-01-15 12:51:31 +0100

    add account view

---
+++ Appetizers/Utilities/Alerts/FormError.swift
@@ -0,0 +1,13 @@
+//
+//  FormError.swift
+//  Appetizers
+//
+//  Created by Francesca Capodanno on 15/01/24.
+//
+
+import Foundation
+
+enum FormError: Error {
+    case invalidForm
+    case invalidEmail
+}
